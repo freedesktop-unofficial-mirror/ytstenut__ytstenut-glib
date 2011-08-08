2ff1a3c0dd0fdf54b1a6f63cb88fc85bbdcb3acb
Rob Staudinger <robsta@linux.intel.com>
2011-08-08 11:31:35 +0200

    Implement YtsgInvocationMessage for invocation marshaling

--- ytstenut-glib/ytsg-invocation-message.c
+++ ytstenut-glib/ytsg-invocation-message.c
@@ -85,6 +85,8 @@ ytsg_invocation_message_new (char const *capability,
   RestXmlNode *node;
 
   node = rest_xml_node_add_child (NULL, "message");
+  /* TODO need those keywords be made reserved */
+  rest_xml_node_add_attr (node, "type", "invocation");
   rest_xml_node_add_attr (node, "capability", capability);
   rest_xml_node_add_attr (node, "aspect", aspect);
 
